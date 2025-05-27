8458e420d933ed5b67cb6db38891db6e22af2c5b
MarcAnt <marc_esq_verb@hotmail.com>
2025-05-26 22:56:31 -0400

    fix: - removed the login protection

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,16 +1,8 @@
 import React from "react";
 import Login from "../components/Login";
 import { Suspense } from "react";
-import { authProfile } from "../utils/authProfile";
-import { redirect } from "next/navigation";
 
 const LoginPage = async () => {
-  const { profile } = await authProfile();
-
-  if (profile) {
-    redirect("/");
-  }
-
   return (
     <Suspense>
       <Login />
